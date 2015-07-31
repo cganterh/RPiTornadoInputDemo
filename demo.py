d22a8c8c7038e0fa7ec541a08b8c664fbe61cc65
Cristóbal Ganter <cganterh@gmail.com>
2015-07-31 19:59:02 +0000

    A light sensor response function was added.

--- demo.py
+++ demo.py
@@ -1,6 +1,22 @@
 from tornado.web import RequestHandler, Application
-from tornado.ioloop import IOLoop
+from tornado.ioloop import IOLoop, PeriodicCallback
 from RPi import GPIO
+
+last_test = 0
+
+
+def test_night():
+    global last_test
+
+    night = GPIO.input(22)
+
+    if night and not last_test:
+        GPIO.output(4, 1)
+
+    if not night and last_test:
+        GPIO.output(4, 0)
+
+    last_test = night
 
 
 class Handler(RequestHandler):
@@ -31,8 +47,17 @@ class Handler(RequestHandler):
 
         self.get()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4, GPIO.OUT)
+    GPIO.setup(22, GPIO.IN)
 
-Application([('/$', Handler)]).listen(53000)
-IOLoop.instance().start()
+    Application([('/$', Handler)]).listen(53000)
+    PeriodicCallback(test_night, 100).start()
+    IOLoop.instance().start()
+
+except KeyboardInterrupt:
+    exit()
+
+finally:
+    GPIO.cleanup()
